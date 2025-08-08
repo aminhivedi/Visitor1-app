91b9febbb92855bbdaa70561021e0578e7528d1f
aminhivedi <amin.hivedi@gmail.com>
2025-08-09 00:10:06 +0330

    Create product.dart

---
+++ lib/data/models/product.dart
@@ -0,0 +1,13 @@
+class Product {
+  final String id;
+  final String name;
+  final int availableStock;
+  final double price;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.availableStock,
+    required this.price,
+  });
+}
